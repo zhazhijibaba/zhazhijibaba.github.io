15270f6ea77d00a40ede2fd2bb35216477ed75c1
榨汁机爸爸实验室 <51279666+zhazhijibaba@users.noreply.github.com>
2023-04-17 19:22:53 -0700

    Update quiz.js

--- pages/test/test1/quiz.js
+++ pages/test/test1/quiz.js
@@ -128,6 +128,72 @@
         e: "<math><mfrac><mi>10</mi><mi>1</mi></mfrac></math>"
       },
       correctAnswer: "a"
+    },    
+    {
+      question: "Compute: <math><mi>8</mi><mo>&times</mo><mi>(-4)</mi><mo>&times</mo><mi>(-3)</mi></math>",
+      answers: {
+        a: "-32",
+        b: "-27",
+        c: "96",
+        d: "32",
+        e: "27"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "Simplify: <math><mi>6</mi><mo>+</mo><msup><mi>(-1)</mi><mi>2</mi></msup><mo>&times</mo><mi>(8+2)</mi></math>",
+      answers: {
+        a: "16",
+        b: "15",
+        c: "50",
+        d: "60",
+        e: "70"
+      },
+      correctAnswer: "a"
+    },    
+    {
+      question: "Yesterday Jaaron ran 5.1 kilometers. Today he ran 2.01 kilometers. How many more kilometers did he run yesterday than today?",
+      answers: {
+        a: "1.40",
+        b: "1.50",
+        c: "2.09",
+        d: "3.09",
+        e: "3.11"
+      },
+      correctAnswer: "d"
+    },    
+    {
+      question: "Karen has <math><mfrac><mi>1</mi><mi>3</mi></mfrac></math> of a pie and Mara has <math><mfrac><mi>1</mi><mi>4</mi></mfrac></math> of the pie. What is the total fraction of the pie that Karen and Mara have?",
+      answers: {
+        a: "<math><mfrac><mi>1</mi><mi>6</mi></mfrac></math>",
+        b: "<math><mfrac><mi>1</mi><mi>2</mi></mfrac></math>",
+        c: "<math><mfrac><mi>7/mi><mi>12</mi></mfrac></math>",
+        d: "<math><mfrac><mi>5</mi><mi>6</mi></mfrac></math>",
+        e: "<math><mfrac><mi>4</mi><mi>3</mi></mfrac></math>"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "A bookstore receives a shipment of 120 books. Of the books,  <math><mfrac><mi>1</mi><mi>3</mi></mfrac></math> are hardcover, and the rest are paperback. Of the paperback books, <math><mfrac><mi>3</mi><mi>4</mi></mfrac></math> are fiction. How many paperback fiction books are in the shipment?",
+      answers: {
+        a: "20",
+        b: "30",
+        c: "40",
+        d: "60",
+        e: "80"
+      },
+      correctAnswer: "d"
+    }, 
+    {
+      question: "An elevator in a building is currently on floor 21. After the elevator goes up 8 fllors, down 5 floors, the up 4 floors, on which floor will the elevator be located?",
+      answers: {
+        a: "9",
+        b: "14",
+        c: "20",
+        d: "30",
+        e: "28"
+      },
+      correctAnswer: "e"
     }
   ];
 
