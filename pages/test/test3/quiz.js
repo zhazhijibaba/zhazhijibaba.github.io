0dfe61f847cb61f77ed63a65c142b1fdd1004291
榨汁机爸爸实验室 <51279666+zhazhijibaba@users.noreply.github.com>
2023-04-19 14:41:27 -0700

    Update quiz.js

--- pages/test/test3/quiz.js
+++ pages/test/test3/quiz.js
@@ -75,125 +75,136 @@
   const submitButton = document.getElementById('submit');
   const myQuestions = [
     {
-      question: "Which of the following decimals is the greatest?",
+      question: "Which of the following is divisible by 15?",
       answers: {
-        a: "1.045",
-        b: "1.456",
-        c: "1.645",
-        d: "1.654",
-        e: "1.065"
-      },
-      correctAnswer: "d"
-    },
-    {
-      question: "Calculate: 1.1 + 20.3 + 4.97",
-      answers: {
-        a: "7.11",
-        b: "26.37",
-        c: "25.47",
-        d: "8.10",
-        e: "71.1"
+        a: "159",
+        b: "225",
+        c: "415",
+        d: "560",
+        e: "663"
       },
       correctAnswer: "b"
     },
     {
-      question: "What is the value of 2.1 x 1.5 ?",
+      question: "Which of the following is the least common multiple of 18, 24, 60?",
       answers: {
-        a: "3150",
-        b: "315",
-        c: "31.5",
-        d: "3.15",
-        e: "0.315"
+        a: "6",
+        b: "72",
+        c: "120",
+        d: "360",
+        e: "720"
       },
       correctAnswer: "d"
     },
     {
-      question: "Compute: <math><mfrac><mi>3</mi><mi>2</mi></mfrac></math> + <math><mfrac><mi>2</mi><mi>5</mi></mfrac></math> - <math><mfrac><mi>1</mi><mi>4</mi></mfrac></math>",
+      question: "How many prime numbers are between 30 and 60?",
       answers: {
-        a: "<math><mfrac><mi>1</mi><mi>5</mi></mfrac></math>",
-        b: "<math><mfrac><mi>4</mi><mi>11</mi></mfrac></math>",
-        c: "<math><mfrac><mi>33</mi><mi>20</mi></mfrac></math>",
-        d: "<math><mfrac><mi>27</mi><mi>20</mi></mfrac></math>",
-        e: "<math><mfrac><mi>4</mi><mi>3</mi></mfrac></math>"
+        a: "7",
+        b: "8",
+        c: "9",
+        d: "10",
+        e: "11"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "If k is an odd number, which of the following is an even number?",
+      answers: {
+        a: "k + 2",
+        b: "k - 10",
+        c: "3k + 1",
+        d: "4k - 1",
+        e: "k(k+4)"
       },
       correctAnswer: "c"
     },
     {
-      question: "What is the value of <math><mfrac><mi>5</mi><mi>3</mi></mfrac> <mo>&divide</mo> <mfrac><mi>3</mi><mi>2</mi></mfrac></math> ?",
+      question: "Which of the following is NOT a factor of <math><mi>60</mi><mo>&times</mo><mi>33</mi></math>",
       answers: {
-        a: "<math><mfrac><mi>10</mi><mi>9</mi></mfrac></math>",
-        b: "<math><mfrac><mi>1</mi><mi>10</mi></mfrac></math>",
-        c: "<math><mfrac><mi>9</mi><mi>10</mi></mfrac></math>",
-        d: "<math><mfrac><mi>5</mi><mi>2</mi></mfrac></math>",
-        e: "<math><mfrac><mi>10</mi><mi>1</mi></mfrac></math>"
-      },
-      correctAnswer: "a"
-    },    
-    {
-      question: "Compute: <math><mi>8</mi><mo>&times</mo><mi>(-4)</mi><mo>&times</mo><mi>(-3)</mi></math>",
-      answers: {
-        a: "-32",
-        b: "-27",
-        c: "96",
-        d: "32",
-        e: "27"
-      },
-      correctAnswer: "c"
-    },
-    {
-      question: "Simplify: <math><mi>6</mi><mo>+</mo><msup><mi>(-1)</mi><mi>2</mi></msup><mo>&times</mo><mi>(8+2)</mi></math>",
-      answers: {
-        a: "16",
-        b: "15",
-        c: "50",
-        d: "60",
-        e: "70"
-      },
-      correctAnswer: "a"
-    },    
-    {
-      question: "Yesterday Jaaron ran 5.1 kilometers. Today he ran 2.01 kilometers. How many more kilometers did he run yesterday than today?",
-      answers: {
-        a: "1.40",
-        b: "1.50",
-        c: "2.09",
-        d: "3.09",
-        e: "3.11"
+        a: "12",
+        b: "20",
+        c: "36",
+        d: "40",
+        e: "55"
       },
       correctAnswer: "d"
-    },    
-    {
-      question: "Karen has <math><mfrac><mi>1</mi><mi>3</mi></mfrac></math> of a pie and Mara has <math><mfrac><mi>1</mi><mi>4</mi></mfrac></math> of the pie. What is the total fraction of the pie that Karen and Mara have?",
-      answers: {
-        a: "<math><mfrac><mi>1</mi><mi>6</mi></mfrac></math>",
-        b: "<math><mfrac><mi>1</mi><mi>2</mi></mfrac></math>",
-        c: "<math><mfrac><mi>7</mi><mi>12</mi></mfrac></math>",
-        d: "<math><mfrac><mi>5</mi><mi>6</mi></mfrac></math>",
-        e: "<math><mfrac><mi>4</mi><mi>3</mi></mfrac></math>"
-      },
-      correctAnswer: "c"
     },
     {
-      question: "A bookstore receives a shipment of 120 books. Of the books,  <math><mfrac><mi>1</mi><mi>3</mi></mfrac></math> are hardcover, and the rest are paperback. Of the paperback books, <math><mfrac><mi>3</mi><mi>4</mi></mfrac></math> are fiction. How many paperback fiction books are in the shipment?",
+      question: "A school reports a student-to-teacher ratio of 6:1. How many students does the school have, if there are 45 teachers in the school?",
       answers: {
-        a: "20",
-        b: "30",
-        c: "40",
-        d: "60",
+        a: "285",
+        b: "270",
+        c: "225",
+        d: "151",
+        e: "51"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "A train traveled 100 miles from City W to City X in 2 hours. The train then traveled 120 miles from City X to City Y in 80 minutes. What was the average speed, in miles per hour, at which the train traveled from City W to City Y?",
+      answers: {
+        a: "53",
+        b: "66",
+        c: "70",
+        d: "73",
         e: "80"
       },
-      correctAnswer: "d"
-    }, 
+      correctAnswer: "b"
+    },
     {
-      question: "An elevator in a building is currently on floor 21. After the elevator goes up 8 fllors, down 5 floors, the up 4 floors, on which floor will the elevator be located?",
+      question: "A length of 100 meters is closest to which of the following lengths, in inches? (1 inch is approximately equal to 2.5 centimeters.)",
       answers: {
-        a: "9",
-        b: "14",
-        c: "20",
-        d: "30",
-        e: "28"
+        a: "40",
+        b: "250",
+        c: "400",
+        d: "2500",
+        e: "4000"
       },
       correctAnswer: "e"
+    },
+    {
+      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $20.40. Tax is an additional 5% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
+      answers: {
+        a: "$10.33",
+        b: "$12.64",
+        c: "$12.75",
+        d: "$21.42",
+        e: "$25.50"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "After changing the number 20 by a certain percent, the result is 50. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 40%",
+        b: "An increase of 30%",
+        c: "An increase of 60%",
+        d: "An increase of 150%",
+        e: "An increase of 250%"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "Of the following, which is closest to <math><mi>110.2</mi><mo>&times</mo><mi>39.998</mi></math>?",
+      answers: {
+        a: "8000",
+        b: "4400",
+        c: "4000",
+        d: "3300",
+        e: "3000"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "Each morning from Monday through Friday, Mr. Ruiz travels 19.8 miles to work. After work, he travels 10.1 miles to a local gym. Then he travels home a distance of 9.9 miles. Of the following, which is closest to the total number of miles Mr. Ruiz travels Monday through Friday?",
+      answers: {
+        a: "100",
+        b: "150",
+        c: "200",
+        d: "250",
+        e: "300"
+      },
+      correctAnswer: "c"
     }
   ];
 
