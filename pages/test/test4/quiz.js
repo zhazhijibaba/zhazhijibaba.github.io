5db2f4e6a9241f0060b72e8483ee51c813db4aa3
榨汁机爸爸实验室 <51279666+zhazhijibaba@users.noreply.github.com>
2023-04-20 17:29:04 -0700

    add quiz4

--- pages/test/test4/quiz.js
+++ pages/test/test4/quiz.js
@@ -75,136 +75,180 @@
   const submitButton = document.getElementById('submit');
   const myQuestions = [
     {
-      question: "Which of the following is divisible by 15?",
+      question: "Which of the following is the least common multiple of 60 and 70?",
       answers: {
-        a: "159",
-        b: "225",
-        c: "415",
-        d: "560",
-        e: "663"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "Which of the following is the least common multiple of 18, 24, 60?",
-      answers: {
-        a: "6",
-        b: "72",
-        c: "120",
-        d: "360",
-        e: "720"
-      },
-      correctAnswer: "d"
-    },
-    {
-      question: "How many prime numbers are between 30 and 60?",
-      answers: {
-        a: "7",
-        b: "8",
-        c: "9",
-        d: "10",
-        e: "11"
-      },
-      correctAnswer: "a"
-    },
-    {
-      question: "If k is an odd number, which of the following is an even number?",
-      answers: {
-        a: "k + 2",
-        b: "k - 10",
-        c: "3k + 1",
-        d: "4k - 1",
-        e: "k(k+4)"
+        a: "60",
+        b: "210",
+        c: "420",
+        d: "630",
+        e: "840"
       },
       correctAnswer: "c"
     },
     {
-      question: "Which of the following is NOT a factor of <math><mi>60</mi><mo>&times</mo><mi>33</mi></math>",
+      question: "Which of the following is the least common multiple of 8, 15 and 30?",
       answers: {
-        a: "12",
-        b: "20",
-        c: "36",
-        d: "40",
-        e: "55"
+        a: "8",
+        b: "30",
+        c: "60",
+        d: "120",
+        e: "240"
       },
       correctAnswer: "d"
     },
     {
-      question: "A school reports a student-to-teacher ratio of 6:1. How many students does the school have, if there are 45 teachers in the school?",
+      question: "How many prime numbers are between 1 and 10?",
       answers: {
-        a: "285",
-        b: "270",
-        c: "225",
-        d: "151",
-        e: "51"
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        e: "5"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "How many prime numbers are between 10 and 20?",
+      answers: {
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        e: "5"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "How many prime numbers are between 20 and 30?",
+      answers: {
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        e: "5"
       },
       correctAnswer: "b"
     },
     {
-      question: "A train traveled 100 miles from City W to City X in 2 hours. The train then traveled 120 miles from City X to City Y in 80 minutes. What was the average speed, in miles per hour, at which the train traveled from City W to City Y?",
+      question: "How many prime numbers are between 30 and 40?",
       answers: {
-        a: "53",
-        b: "66",
-        c: "70",
-        d: "73",
-        e: "80"
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        e: "5"
       },
       correctAnswer: "b"
     },
     {
-      question: "A length of 100 meters is closest to which of the following lengths, in inches? (1 inch is approximately equal to 2.5 centimeters.)",
+      question: "Which of the following is NOT a factor of <math><mi>20</mi><mo>&times</mo><mi>80</mi></math>",
       answers: {
-        a: "40",
-        b: "250",
-        c: "400",
-        d: "2500",
-        e: "4000"
+        a: "64",
+        b: "25",
+        c: "50",
+        d: "160",
+        e: "120"
       },
       correctAnswer: "e"
     },
     {
-      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $20.40. Tax is an additional 5% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
+      question: "Which of the following is NOT a factor of <math><mi>18</mi><mo>&times</mo><mi>30</mi></math>",
       answers: {
-        a: "$10.33",
-        b: "$12.64",
-        c: "$12.75",
-        d: "$21.42",
-        e: "$25.50"
+        a: "27",
+        b: "36",
+        c: "40",
+        d: "54",
+        e: "90"
       },
       correctAnswer: "c"
     },
     {
-      question: "After changing the number 20 by a certain percent, the result is 50. Which of the following represents this percent change?",
+      question: "Which of the following is NOT a factor of <math><mi>30</mi><mo>&times</mo><mi>35</mi></math>",
       answers: {
-        a: "A decrease of 40%",
-        b: "An increase of 30%",
-        c: "An increase of 60%",
-        d: "An increase of 150%",
-        e: "An increase of 250%"
+        a: "80",
+        b: "70",
+        c: "75",
+        d: "50",
+        e: "42"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "A train traveled 100 miles from City W to City X in 4 hours. The train then traveled 105 miles from City X to City Y in 180 minutes. What was the average speed, in miles per hour, at which the train traveled from City W to City Y?",
+      answers: {
+        a: "20",
+        b: "25",
+        c: "30",
+        d: "35",
+        e: "40"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "A boy walked 10 miles from home to school in 2 hours. The boy then walked 6 miles from school to  library in 3 hours. What was the average speed, in miles per hour, at which the boy walked from home to library?",
+      answers: {
+        a: "2",
+        b: "2.5",
+        c: "3",
+        d: "3.5",
+        e: "5"
       },
       correctAnswer: "d"
     },
     {
-      question: "Of the following, which is closest to <math><mi>110.2</mi><mo>&times</mo><mi>39.998</mi></math>?",
+      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $10.40. Tax is an additional 5% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
       answers: {
-        a: "8000",
-        b: "4400",
-        c: "4000",
-        d: "3300",
-        e: "3000"
-      },
-      correctAnswer: "b"
-    },
-    {
-      question: "Each morning from Monday through Friday, Mr. Ruiz travels 19.8 miles to work. After work, he travels 10.1 miles to a local gym. Then he travels home a distance of 9.9 miles. Of the following, which is closest to the total number of miles Mr. Ruiz travels Monday through Friday?",
-      answers: {
-        a: "100",
-        b: "150",
-        c: "200",
-        d: "250",
-        e: "300"
+        a: "$5.20",
+        b: "$6.24",
+        c: "$6.50",
+        d: "$12.48",
+        e: "$13.00"
       },
       correctAnswer: "c"
+    },
+    {
+      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $20.40. Tax is an additional 10% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
+      answers: {
+        a: "$10.20",
+        b: "$11.22",
+        c: "$12.24",
+        d: "$13.26",
+        e: "$26.52"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "After changing the number 20 by a certain percent, the result is 40. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 50%",
+        b: "An increase of 50%",
+        c: "An increase of 100%",
+        d: "An increase of 200%",
+        e: "An increase of 400%"
+      },
+      correctAnswer: "c"
+    }
+    {
+      question: "After changing the number 40 by a certain percent, the result is 50. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 10%",
+        b: "An increase of 10%",
+        c: "An increase of 20%",
+        d: "An increase of 25%",
+        e: "An increase of 30%"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "After changing the number 50 by a certain percent, the result is 40. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 10%",
+        b: "A decrease of 20%",
+        c: "A decrease of 25%",
+        d: "A decrease of 30%",
+        e: "An increase of 10%"
+      },
+      correctAnswer: "b"
     }
   ];
 
