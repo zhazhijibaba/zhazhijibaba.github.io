15648ce96cfe2d0620d27a3a148cb8a17779b036
榨汁机爸爸实验室 <51279666+zhazhijibaba@users.noreply.github.com>
2023-04-20 17:17:25 -0700

    Update quiz.js

--- pages/test/test4/quiz.js
+++ pages/test/test4/quiz.js
@@ -75,48 +75,81 @@
   const submitButton = document.getElementById('submit');
   const myQuestions = [
     {
-      question: "Which of the following is the least common multiple of 60 and 70?",
+      question: "A train traveled 100 miles from City W to City X in 4 hours. The train then traveled 105 miles from City X to City Y in 180 minutes. What was the average speed, in miles per hour, at which the train traveled from City W to City Y?",
       answers: {
-        a: "60",
-        b: "210",
-        c: "420",
-        d: "630",
-        e: "840"
+        a: "20",
+        b: "25",
+        c: "30",
+        d: "35",
+        e: "40"
       },
       correctAnswer: "c"
     },
     {
-      question: "Which of the following is the least common multiple of 8, 15 and 30?",
+      question: "A boy walked 10 miles from home to school in 2 hours. The boy then walked 6 miles from school to  library in 3 hours. What was the average speed, in miles per hour, at which the boy walked from home to library?",
       answers: {
-        a: "8",
-        b: "30",
-        c: "60",
-        d: "120",
-        e: "240"
+        a: "2",
+        b: "2.5",
+        c: "3",
+        d: "3.5",
+        e: "5"
       },
       correctAnswer: "d"
     },
     {
-      question: "Which of the following is the least common multiple of 60 and 70?",
+      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $10.40. Tax is an additional 5% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
       answers: {
-        a: "60",
-        b: "210",
-        c: "420",
-        d: "630",
-        e: "840"
+        a: "$5.20",
+        b: "$6.24",
+        c: "$6.50",
+        d: "$12.48",
+        e: "$13.00"
       },
       correctAnswer: "c"
     },
     {
-      question: "Which of the following is the least common multiple of 8, 15 and 30?",
+      question: "Two friends went out to lunch and split the cost equally. The meal's cost was $20.40. Tax is an additional 10% on the meal's cost. The friends decide to leave 20% of the meal's cost(not including tax) for a tip. How much will each friend spend on the meal?",
       answers: {
-        a: "8",
-        b: "30",
-        c: "60",
-        d: "120",
-        e: "240"
+        a: "$10.20",
+        b: "$11.22",
+        c: "$12.24",
+        d: "$13.26",
+        e: "$26.52"
       },
       correctAnswer: "d"
+    },
+    {
+      question: "After changing the number 20 by a certain percent, the result is 40. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 50%",
+        b: "An increase of 50%",
+        c: "An increase of 100%",
+        d: "An increase of 200%",
+        e: "An increase of 400%"
+      },
+      correctAnswer: "c"
+    }
+    {
+      question: "After changing the number 40 by a certain percent, the result is 50. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 10%",
+        b: "An increase of 10%",
+        c: "An increase of 20%",
+        d: "An increase of 25%",
+        e: "An increase of 30%"
+      },
+      correctAnswer: "d"
+    },
+    {
+      question: "After changing the number 50 by a certain percent, the result is 40. Which of the following represents this percent change?",
+      answers: {
+        a: "A decrease of 10%",
+        b: "A decrease of 20%",
+        c: "A decrease of 25%",
+        d: "A decrease of 30%",
+        e: "An increase of 10%"
+      },
+      correctAnswer: "b"
     }
   ];
 
